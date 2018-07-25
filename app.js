8037663331ab3650f05e4e578c7a8fe9dd1681da
Marcin Szuster <szust.marcin@gmail.com>
2018-07-25 15:43:51 +0200

    Changed indents to tabs

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	indentedSyntax: true, // true = .sass and false = .scss
+	sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
