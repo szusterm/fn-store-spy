1ecbcfde8a9f5395a503eab0a179dcb7741a0a42
Marcin Szuster <szust.marcin@gmail.com>
2018-07-28 00:22:02 +0200

    Added new shop getters

--- bin/shop.js
+++ bin/shop.js
@@ -1,8 +1,30 @@
 class Shop {
-	get() {
-		return [{
-			name: 'Battle Hound'
-		}];
+	constructor() {
+		this._items = [];
+	}
+
+	update() {
+		this._items = [{name: 'Battle Hound'}];
+	}
+
+	get items() {
+		return this._items;
+	}
+
+	get ids() {
+		const ids = [];
+		for (const {id} of this._items) {
+			ids.push(id);
+		}
+		return ids;
+	}
+
+	get names() {
+		const names = [];
+		for (const {name} of this._items) {
+			names.push(name);
+		}
+		return names;
 	}
 }
 
