4fb92b4176c57273757ddb0ae2d552f57885e3b8
Marcin Szuster <szust.marcin@gmail.com>
2018-07-27 17:21:31 +0200

    Changed item factory function to expanded class

---
+++ database/factories/item.factory.js
@@ -0,0 +1,34 @@
+const ItemModel = require('../models/item');
+const getObject = require('../../helpers/getResponseObject');
+
+class Item {
+	async find(data) {
+		try {
+			const query = ItemModel.find(data);
+			const response = await query.exec();
+
+			return getObject(false, response);
+		}
+		catch (error) {
+			return getObject(true, error);
+		}
+	}
+
+	async findById(ids) {
+		const data = {
+			_id: {$in: ids}
+		};
+
+		return await this.find(data);
+	}
+
+	async findByName(names) {
+		const data = {
+			name: {$in: names}
+		};
+
+		return await this.find(data);
+	}
+}
+
+module.exports = () => new Item();
