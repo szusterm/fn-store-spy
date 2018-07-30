ee1dc9917a8b84851684d3b62599cb194b8bad2c
Marcin Szuster <szust.marcin@gmail.com>
2018-07-30 14:08:13 +0200

    Added base item factory filters

--- models/item/item.factory.js
+++ models/item/item.factory.js
@@ -2,6 +2,37 @@ const ItemModel = require('./item.model');
 const getObject = require('../../helpers/getResponseObject');
 
 class Item {
+	constructor() {
+		this._findQuery = null;
+	}
+
+	async find() {
+
+	}
+
+	_getFilterFunctions() {
+		return {
+			ids: this._filterByIds,
+			name: this._filterByName,
+			type: this._filterByType
+		}
+	}
+
+	_filterByIds(ids) {
+		this._findQuery.where('_id').in(ids);
+		return this._getFilterFunctions();
+	}
+
+	_filterByName(name) {
+		//here
+		this._findQuery.where('name').equals()
+	}
+
+	_filterByType(type) {
+		this._findQuery.where('type').equals(type);
+		return this._getFilterFunctions();
+	}
+
 	async _find(data) {
 		try {
 			const query = ItemModel.find(data);
