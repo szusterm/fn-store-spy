2463581b6336c4080edfd3209f98cc95ee5dd050
Marcin Szuster <szust.marcin@gmail.com>
2018-07-30 13:11:44 +0200

    Removed error data from response

--- routes/order.js
+++ routes/order.js
@@ -5,9 +5,9 @@ const order = require('../models/order');
 
 router.post('/add', async (req, res) => {
 	const {items} = req.body;
-	const dbResponse = await order.add(items, 'code');
+	const {err, data} = await order.add(items, 'code');
 
-	res.json(dbResponse);
+	res.json((err) ? {err} : {err, data});
 });
 
 module.exports = router;
