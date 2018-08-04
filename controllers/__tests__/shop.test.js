208f1b0a06990ae04f4435d5bf66564b6b9bddae
Marcin Szuster <szust.marcin@gmail.com>
2018-08-04 23:32:48 +0200

    Added shop.update() test

---
+++ controllers/__tests__/shop.test.js
@@ -0,0 +1,51 @@
+const shop = require('../shop');
+const itemFactory = require('../../models/item');
+
+jest.mock('../../models/item', () => jest.fn());
+
+describe('Shop Controller', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	describe('update()', () => {
+		let mockUpdateItemsWithDatabase;
+		let mockGetNamesFromObjects;
+		beforeEach(() => {
+			mockUpdateItemsWithDatabase = jest.spyOn(shop, '_updateItemsWithDatabase');
+			mockGetNamesFromObjects = jest.spyOn(shop, '_getNamesFromObjects');
+		});
+		afterEach(() => {
+			mockUpdateItemsWithDatabase.mockRestore();
+			mockGetNamesFromObjects.mockRestore();
+		});
+
+		it('update property to current fortnite shop offer', async () => {
+			const exampleItems = [
+				{name: 'Some Axe'},
+				{name: 'Super Backapck'}
+			];
+			const itemsNamesArray = [
+				exampleItems[0].name,
+				exampleItems[1].name
+			];
+
+			shop._items = [];
+			mockUpdateItemsWithDatabase.mockImplementationOnce(() => {
+				shop._items = exampleItems;
+				return true;
+			});
+			mockGetNamesFromObjects.mockReturnValueOnce(itemsNamesArray);
+
+			const response = await shop.update();
+
+			expect(shop._items).toEqual(exampleItems);
+			expect(response).toBeTruthy();
+			expect(mockUpdateItemsWithDatabase).toHaveBeenCalledWith(itemsNamesArray);
+		});
+	});
+
+	describe('_updateItemsWithDatabase', () => {
+
+	});
+});
