bbef1549d6ea4bcd2490544328630001c00f21fb
Marcin Szuster <szust.marcin@gmail.com>
2018-08-05 13:48:27 +0200

    Added new methods to item factory mock

--- database/factories/__mocks__/item.js
+++ database/factories/__mocks__/item.js
@@ -1,7 +1,11 @@
 const item = {
 	find: jest.fn(() => item),
 	exec: jest.fn(() => item),
-	names: jest.fn(() => item)
+	ids: jest.fn(() => item),
+	name: jest.fn(() => item),
+	names: jest.fn(() => item),
+	type: jest.fn(() => item),
+	page: jest.fn(() => item)
 };
 
 module.exports = item;
