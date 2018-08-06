4fb1b34a84ac4adedb47057256a789719739e9b4
Marcin Szuster <szust.marcin@gmail.com>
2018-08-06 13:09:02 +0200

    Added ItemCard component

---
+++ src/components/ItemCard/ItemCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './styles.scss';
+
+class ItemCard extends Component {
+	render() {
+		return (
+			<div className='item-card'/>
+		);
+	}
+}
+
+export default ItemCard;
