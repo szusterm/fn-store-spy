fa10b6adcf46f23c8a17ab8aa172657dfe00b68e
Marcin Szuster <szust.marcin@gmail.com>
2018-08-06 20:03:15 +0200

    Added ItemsList to showing items

---
+++ src/components/ItemsList/ItemsList.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import ItemCard from '../ItemCard';
+
+import './styles.scss';
+
+class ItemsList extends Component {
+	render() {
+		const {items} = this.props;
+
+		return (
+			<div>
+				{
+					items.map((item) => {
+						const {name, price, rarity, imgSrc} = item;
+
+						return (
+							<ItemCard
+								name={name}
+								price={price}
+								rarity={rarity}
+								imgSrc={imgSrc}
+							/>
+						);
+					})
+				}
+			</div>
+		);
+	}
+}
+
+ItemsList.propTypes = {
+	items: PropTypes.array
+};
+
+export default ItemsList;
