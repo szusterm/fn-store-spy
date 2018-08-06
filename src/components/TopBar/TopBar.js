177a6cf6ef85aadb0a6be0ef6f97cc251662175d
Marcin Szuster <szust.marcin@gmail.com>
2018-08-06 19:11:30 +0200

    Added TopBar component

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './styles.scss';
+
+class TopBar extends Component {
+	render() {
+		return (
+			<div className={'top-bar'}>TopBar</div>
+		);
+	}
+}
+
+export default TopBar;
