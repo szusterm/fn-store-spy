e1c41fa9f7ebe94cde6220eb197f54337269b865
Marcin Szuster <szust.marcin@gmail.com>
2018-08-06 12:09:18 +0200

    Changed src/index to importing react App component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-(() => {
-	console.log('It works!');
-})();
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
