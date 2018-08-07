a1ad9e4d47f4326ac9ef41789d6478a5ac8152e9
Marcin Szuster <szust.marcin@gmail.com>
2018-08-07 15:27:57 +0200

    Added redux to Searching

--- src/components/Searching/Searching.js
+++ src/components/Searching/Searching.js
@@ -1,20 +1,40 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {replaceItems} from '../../redux/actions';
 
 import ItemsList from '../ItemsList';
 
 import './styles.scss';
 
 class Searching extends Component {
+	componentDidMount() {
+		this.props.replaceItems({page: 1});
+	}
+
 	render() {
 		return (
 			<div className={'container'}>
 				<div className={'row'}>
-					<ItemsList/>
+					<ItemsList items={this.props.items}/>
 				</div>
 			</div>
 		);
 	}
 }
 
-export default Searching;
+Searching.propTypes = {
+	replaceItems: PropTypes.func, //redux
+	items: PropTypes.array //redux
+};
+
+
+const mapStateToProps = (state) => {
+	const {items} = state;
+
+	return {items};
+};
+
+const mapDispatchToProps = {replaceItems};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Searching);
