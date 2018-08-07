d53b6524e857987146439e579ea1495fa4b6911f
Marcin Szuster <szust.marcin@gmail.com>
2018-08-07 12:16:20 +0200

    Added reducer for items list

---
+++ src/redux/reducers/itemsList.js
@@ -0,0 +1,10 @@
+const reducer = (state = [], action) => {
+	switch(action.type) {
+		case 'FETCH_ITEMS':
+			return [...action.items];
+		default:
+			return state;
+	}
+};
+
+export default reducer;
