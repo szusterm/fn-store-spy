2f8ce56f06c9f6fdcda29fa25d4159f89257f8cb
Marcin Szuster <szust.marcin@gmail.com>
2018-08-07 10:43:25 +0200

    Added Searching component

---
+++ src/components/Searching/Searching.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Searching extends Component {
+	render() {
+		return (
+			<div/>
+		);
+	}
+}
+
+export default Searching;
