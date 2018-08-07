c31b2cb196445a96d4e6b5e7eceba5aead87a34b
Marcin Szuster <szust.marcin@gmail.com>
2018-08-07 10:53:02 +0200

    Added base theme in Searching

--- src/components/Searching/Searching.js
+++ src/components/Searching/Searching.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import ItemsList from '../ItemsList';
+
+import './styles.scss';
+
 class Searching extends Component {
 	render() {
 		return (
-			<div/>
+			<div className={'container'}>
+				<div className={'row'}>
+					<ItemsList/>
+				</div>
+			</div>
 		);
 	}
 }
