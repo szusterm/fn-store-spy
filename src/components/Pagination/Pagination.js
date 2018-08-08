75992021768a93a2e4887c83df5085d495a80b6f
Marcin Szuster <szust.marcin@gmail.com>
2018-08-08 15:36:01 +0200

    Added Pagination component

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+class Pagination extends Component {
+	render() {
+		return (
+			<div/>
+		);
+	}
+}
+
+export default Pagination;
