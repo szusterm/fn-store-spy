99cd9c18257116336d1ad4a52c87e0d642aae3a7
Marcin Szuster <szust.marcin@gmail.com>
2018-08-08 17:11:57 +0200

    Added changing pages in pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -4,17 +4,54 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 class Pagination extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			page: 1
+		};
+
+		this.previousPage = this.previousPage.bind(this);
+		this.nextPage = this.nextPage.bind(this);
+	}
+
+	updatePage(newPage = 1) {
+		const newState = this.state;
+		newState.page = newPage;
+		this.setState(newState);
+
+		this.props.onChange(newState.page);
+	}
+
+	nextPage() {
+		const {page} = this.state;
+		const newPage = page + 1;
+		this.updatePage(newPage);
+	}
+
+	previousPage() {
+		const {page} = this.state;
+		const newPage = page - 1;
+		this.updatePage(newPage);
+	}
+
 	render() {
 		return (
 			<div className={'container-fluid pagination'}>
 				<div className={'row'}>
 					<div className={'col-6'}>
-						<button className={'pagination--button pagination--button--previous'}>
+						<button
+							className={'pagination--button pagination--button--previous'}
+							onClick={this.previousPage}
+						>
 							Previous
 						</button>
 					</div>
 					<div className={'col-6'}>
-						<button className={'pagination--button pagination--button--next'}>
+						<button
+							className={'pagination--button pagination--button--next'}
+							onClick={this.nextPage}
+						>
 							Next
 						</button>
 					</div>
@@ -23,5 +60,9 @@ class Pagination extends Component {
 		);
 	}
 }
+
+Pagination.propTypes = {
+	onChange: PropTypes.func
+};
 
 export default Pagination;
