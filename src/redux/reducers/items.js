7d5b154feeeb9d1f5fedc506d14dd75470f7431a
Marcin Szuster <szust.marcin@gmail.com>
2018-08-09 23:39:58 +0200

    Fixed items reducer

--- src/redux/reducers/items.js
+++ src/redux/reducers/items.js
@@ -13,12 +13,12 @@ const reducer = (state = initialState, action) => {
 
 		case 'SET_NEXT_PAGE_AVAILABILITY':
 			return Object.assign({}, state, {
-				nextPageAvailable: action.nextPageAvailable
+				nextPageAvailable: action.data
 			});
 
 		case 'SET_MAX_ITEMS_PER_PAGE':
 			return Object.assign({}, state, {
-				maxItemsPerPage: action.maxItemsPerPage
+				maxItemsPerPage: action.data
 			});
 
 		default:
