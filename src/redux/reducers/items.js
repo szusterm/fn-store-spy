6450f26098fdeb6931d1afaeca4ef95892832782
Marcin Szuster <szust.marcin@gmail.com>
2018-08-09 23:15:56 +0200

    Added new actions to items reducer

--- src/redux/reducers/items.js
+++ src/redux/reducers/items.js
@@ -1,5 +1,7 @@
 const initialState = {
-	list: []
+	list: [],
+	nextPageAvailable: false,
+	maxItemsPerPage: 0
 };
 
 const reducer = (state = initialState, action) => {
@@ -8,6 +10,17 @@ const reducer = (state = initialState, action) => {
 			return Object.assign({}, state, {
 				list: action.items
 			});
+
+		case 'SET_NEXT_PAGE_AVAILABILITY':
+			return Object.assign({}, state, {
+				nextPageAvailable: action.nextPageAvailable
+			});
+
+		case 'SET_MAX_ITEMS_PER_PAGE':
+			return Object.assign({}, state, {
+				maxItemsPerPage: action.maxItemsPerPage
+			});
+
 		default:
 			return state;
 	}
