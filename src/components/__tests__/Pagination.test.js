1425bf986b42de8e6b244917035c1cd18d376faf
Marcin Szuster <szust.marcin@gmail.com>
2018-08-16 18:09:43 +0200

    Added new Pagination tests

--- src/components/__tests__/Pagination.test.js
+++ src/components/__tests__/Pagination.test.js
@@ -4,17 +4,17 @@ import {shallow} from 'enzyme';
 import {Pagination} from '../Pagination/Pagination';
 
 const setup = (propOverrides, stateOverrides) => {
-		const props = Object.assign({
-			onChange: () => true
-		}, propOverrides);
+	const props = Object.assign({
+		onChange: () => true
+	}, propOverrides);
 
-		const state = Object.assign({
-			nextPageAvailable: true
-		}, stateOverrides);
+	const state = Object.assign({
+		nextPageAvailable: true
+	}, stateOverrides);
 
-		const wrapper = shallow(<Pagination {...state} {...props}/>);
+	const wrapper = shallow(<Pagination {...state} {...props}/>);
 
-		return {props, state, wrapper};
+	return {props, state, wrapper};
 };
 
 describe('Pagination Component', () => {
@@ -38,5 +38,29 @@ describe('Pagination Component', () => {
 		wrapper.setState({page: startPage});
 		wrapper.find('.pagination--button--next').simulate('click');
 		expect(props.onChange).toHaveBeenCalledWith(startPage + 1);
+	});
+
+	it('makes previous button disabled, if page equals to 1', () => {
+		const {wrapper} = setup();
+
+		wrapper.setState({page: 1});
+		const isDisabledWithLower = wrapper.find('.pagination--button--previous').props().disabled;
+		expect(isDisabledWithLower).toBe(true);
+
+		wrapper.setState({page: 2});
+		const isDisabledWithHigher = wrapper.find('.pagination--button--previous').props().disabled;
+		expect(isDisabledWithHigher).toBe(false);
+	});
+
+	it('makes next button disabled, if next page does not exists', () => {
+		const {wrapper} = setup();
+
+		wrapper.setProps({nextPageAvailable: false});
+		const isDisabledWithoutNextPage = wrapper.find('.pagination--button--next').props().disabled;
+		expect(isDisabledWithoutNextPage).toBe(true);
+
+		wrapper.setProps({nextPageAvailable: true});
+		const isDisabledWithNextPage = wrapper.find('.pagination--button--next').props().disabled;
+		expect(isDisabledWithNextPage).toBe(false);
 	});
 });
