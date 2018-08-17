73b747b82ccee5aab43889f360cd463761b848df
Marcin Szuster <szust.marcin@gmail.com>
2018-08-17 20:04:02 +0200

    Added getting config to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import {setConfig} from './redux/actions';
 
 import Searching from './components/Searching';
 
 import './scss/index.scss';
 
 class App extends Component {
+	componentDidMount() {
+		this.props.setConfig();
+	}
+
 	render() {
 		return (
 			<Searching/>
@@ -12,4 +20,10 @@ class App extends Component {
 	}
 }
 
-export default App;
+App.propTypes = {
+	setConfig: PropTypes.func //redux
+};
+
+const mapDispatchToProps = {setConfig};
+
+export default connect(null, mapDispatchToProps)(App);
