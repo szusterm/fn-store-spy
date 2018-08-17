c4f53547dc30cbb30af0a7daa58ba96e74d6f66f
Marcin Szuster <szust.marcin@gmail.com>
2018-08-17 19:59:05 +0200

    Fixed reducer action param

--- src/redux/reducers/config.js
+++ src/redux/reducers/config.js
@@ -3,7 +3,7 @@ const initialState = {};
 const reducer = (state = initialState, action) => {
 	switch(action.type) {
 		case 'SET_CONFIG':
-			return {...state};
+			return {...state, ...action.config};
 		default:
 			return state;
 	}
