a3c5a4974dcbb3afcce7874cbdc61ddafa27ae92
Marcin Szuster <szust.marcin@gmail.com>
2018-08-17 16:00:25 +0200

    Added changing name in store in NameSearchBar

--- src/components/NameSearchBar/NameSearchBar.js
+++ src/components/NameSearchBar/NameSearchBar.js
@@ -7,12 +7,27 @@ import {setNameFilter} from '../../redux/actions';
 import './styles.scss';
 
 export class NameSearchBar extends Component {
+	constructor() {
+		super();
+
+		this.updateName = this.updateName.bind(this);
+	}
+
+	updateName(event) {
+		const {value: name} = event.target;
+
+		this.props.setNameFilter(name);
+		this.props.onChange(name);
+	}
+
 	render() {
 		return (
 			<div className={'name-search-bar'}>
 				<input
 					className={'name-search-bar--input'}
 					placeholder={'Search by name'}
+					value={this.props.name}
+					onChange={this.updateName}
 				/>
 			</div>
 		);
@@ -20,6 +35,7 @@ export class NameSearchBar extends Component {
 }
 
 NameSearchBar.propTypes = {
+	onChange: PropTypes.func,
 	name: PropTypes.string, //redux
 	setNameFilter: PropTypes.func //redux
 };
