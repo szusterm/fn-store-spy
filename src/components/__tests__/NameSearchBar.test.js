1a94ac8345cc2bc0b768c4a564615ede701f6971
Marcin Szuster <szust.marcin@gmail.com>
2018-08-19 15:03:29 +0200

    Added default props to NameSearchBar tests setup

--- src/components/__tests__/NameSearchBar.test.js
+++ src/components/__tests__/NameSearchBar.test.js
@@ -4,7 +4,13 @@ import {shallow} from 'enzyme';
 import {NameSearchBar} from '../NameSearchBar/NameSearchBar';
 
 const setup = (propOverrides) => {
-	const props = Object.assign({}, propOverrides);
+	const props = Object.assign({
+		onChange: () => true,
+		name: '', //redux
+		minNameFilterLength: 3, //redux
+		maxNameFilterLength: 26, //redux
+		setNameFilter: () => true //redux
+	}, propOverrides);
 
 	const wrapper = shallow(<NameSearchBar {...props}/>);
 
