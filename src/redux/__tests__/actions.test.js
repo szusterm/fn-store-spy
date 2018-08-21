73b6aa4e90dcd0980af23ef3dedb828796989b7a
Marcin Szuster <szust.marcin@gmail.com>
2018-08-21 20:20:35 +0200

    Added test for setNameFilter() action creator

--- src/redux/__tests__/actions.test.js
+++ src/redux/__tests__/actions.test.js
@@ -17,4 +17,17 @@ describe('Actions', () => {
 			expect(expectedChange).toEqual(returnedChange);
 		});
 	});
+
+	describe('setNameFilter()', () => {
+		it('calls reducer to change a name filter', () => {
+			const expectedChange = {
+				type: types.SET_NAME,
+				payload: 6
+			};
+
+			const returnedChange = actions.setNameFilter(expectedChange.payload);
+
+			expect(expectedChange).toEqual(returnedChange);
+		});
+	});
 });
