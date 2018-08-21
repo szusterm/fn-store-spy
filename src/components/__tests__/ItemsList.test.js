65bed3476ccad601e71dcd6824bcb3ca20730781
Marcin Szuster <szust.marcin@gmail.com>
2018-08-21 14:18:13 +0200

    Fixed error with prop types in ItemsList tests

--- src/components/__tests__/ItemsList.test.js
+++ src/components/__tests__/ItemsList.test.js
@@ -5,15 +5,28 @@ import {ItemsList} from '../ItemsList/ItemsList';
 
 import ItemCard from '../ItemCard';
 
+const exampleItemsList = [
+	{
+		_id: '3h5i34uh53i4',
+		name: 'Super Axe',
+		price: '2000',
+		rarity: 'uncommon',
+		type: 'axe',
+		imageSrc: 'https://example.com'
+	},
+	{
+		_id: '56b7h8678b',
+		name: 'Something',
+		price: '800',
+		rarity: 'rare',
+		type: 'boots',
+		imageSrc: 'https://example.pl'
+	}
+];
+
 const setup = (propOverrides) => {
 	const props = Object.assign({
-		items: [{
-			name: 'Super Axe',
-			price: '2000',
-			rarity: 'uncommon',
-			type: 'axe',
-			imageSrc: 'https://example.com'
-		}]
+		items: exampleItemsList
 	}, propOverrides);
 
 	const wrapper = shallow(<ItemsList {...props}/>);
@@ -30,7 +43,7 @@ describe('ItemsList Component', () => {
 
 	it('shows items of ItemCards from prop', () => {
 		const {wrapper, props} = setup({
-			items: [{}, {}, {}]
+			items: exampleItemsList
 		});
 
 		const items = wrapper.find(ItemCard);
@@ -38,30 +51,21 @@ describe('ItemsList Component', () => {
 		expect(items.length).toBe(props.items.length);
 	});
 
-	it('passes props to every ItemCard', () => {
+	it('passes props without id to every ItemCard', () => {
 		const {wrapper, props} = setup({
-			items: [
-				{
-					name: 'Super Axe',
-					price: '2000',
-					rarity: 'uncommon',
-					type: 'axe',
-					imageSrc: 'https://example.com'
-				},
-				{
-					name: 'Something',
-					price: '800',
-					rarity: 'rare',
-					type: 'boots',
-					imageSrc: 'https://example.pl'
-				}
-			]
+			items: exampleItemsList
 		});
+
+		const firstItemProps = {...props.items[0]};
+		const secondItemProps = {...props.items[1]};
+
+		delete firstItemProps._id;
+		delete secondItemProps._id;
 
 		const firstItemCard = wrapper.find(ItemCard).at(0);
 		const secondItemCard = wrapper.find(ItemCard).at(1);
 
-		expect(firstItemCard.props()).toEqual(props.items[0]);
-		expect(secondItemCard.props()).toEqual(props.items[1]);
+		expect(firstItemCard.props()).toEqual(firstItemProps);
+		expect(secondItemCard.props()).toEqual(secondItemProps);
 	});
 });
