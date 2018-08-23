bd60f826d33d127b8a2147511615d01d20ae9780
Marcin Szuster <szust.marcin@gmail.com>
2018-08-23 18:26:38 +0200

    Added action types for order

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -4,5 +4,9 @@ export const SET_NEXT_PAGE_AVAILABILITY = 'SET_NEXT_PAGE_AVAILABILITY';
 export const SET_PAGE = 'SET_PAGE';
 export const SET_NAME = 'SET_NAME';
 
+//Order
+export const ADD_ITEM_TO_ORDER = 'ADD_ITEM_TO_ORDER';
+export const REMOVE_ITEM_FROM_ORDER = 'REMOVE_ITEM_FROM_ORDER';
+
 //Config
 export const SET_CONFIG = 'SET_CONFIG';
