a10683e5097041e3b0dc392df2d9c6f804fd9956
Marcin Szuster <szust.marcin@gmail.com>
2018-08-23 19:06:42 +0200

    Added order reducer

---
+++ src/redux/reducers/order.js
@@ -0,0 +1,24 @@
+import * as types from '../actionTypes';
+
+const initialState = {
+	items: []
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case types.ADD_ITEM_TO_ORDER:
+			return {
+				...state,
+				items: [...state.items, action.payload]
+			};
+		case types.REMOVE_ITEM_FROM_ORDER:
+			return {
+				...state,
+				items: [...state.items].splice(action.payload - 1, 1)
+			};
+		default:
+			return state;
+	}
+};
+
+export default reducer;
