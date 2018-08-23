235a52328bba286532d07a995dfb9b5062f22da3
Marcin Szuster <szust.marcin@gmail.com>
2018-08-23 19:08:01 +0200

    Imported order reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 
 import searching from './searching';
+import order from './order';
 import config from './config';
 
 export default combineReducers({
 	searching,
+	order,
 	config
 });
