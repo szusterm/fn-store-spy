eb48099c4e9a034addda094caa7528eac46525d7
Marcin Szuster <szust.marcin@gmail.com>
2018-08-23 20:20:01 +0200

    Added putting id as a prop to ItemCard

--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -22,6 +22,7 @@ export class ItemsList extends Component {
 									className={'col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3'}
 								>
 									<ItemCard
+										id={_id}
 										name={name}
 										price={price}
 										rarity={rarity}
