0e5cf16e403aaf91ad7c2fd3e1faa4a24e15d137
Marcin Szuster <szust.marcin@gmail.com>
2018-08-23 17:06:35 +0200

    Added base Order component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+export class Order extends Component {
+	render() {
+		return (
+			<div/>
+		);
+	}
+}
+
+export default Order;
