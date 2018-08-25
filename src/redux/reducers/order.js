e7cf8fe58f2181ec5c83d42da6ba05993b60da1b
Marcin Szuster <szust.marcin@gmail.com>
2018-08-25 11:25:21 +0200

    Changed switch to if in order reducer

--- src/redux/reducers/order.js
+++ src/redux/reducers/order.js
@@ -5,19 +5,27 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case types.ADD_ITEM_TO_ORDER:
-			return {
-				...state,
-				items: [...state.items, action.payload]
-			};
-		case types.SET_ORDERED_ITEM_ACTIVE:
-			state.items[action.payload.index].active = action.payload.active;
-			return {
-				...state
-			};
-		default:
-			return state;
+	const {type, payload} = action;
+
+	if (type === types.ADD_ITEM_TO_ORDER) {
+		return {
+			...state,
+			items: [...state.items, payload]
+		};
+	}
+	else if (type === types.SET_ORDERED_ITEM_ACTIVE){
+		const {index, active} = payload;
+
+		const chnagedItems = [...state.items];
+		chnagedItems[index].active = active;
+
+		return {
+			...state,
+			items: chnagedItems
+		};
+	}
+	else {
+		return state;
 	}
 };
 
