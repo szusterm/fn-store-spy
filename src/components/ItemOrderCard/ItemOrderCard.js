760a7cf850b4a6571ce013a82a62a6db98284a3d
Marcin Szuster <szust.marcin@gmail.com>
2018-08-26 20:36:21 +0200

    Updated ItemOrderCard

--- src/components/ItemOrderCard/ItemOrderCard.js
+++ src/components/ItemOrderCard/ItemOrderCard.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import ItemCoin from '../ItemCoin';
+
 import './styles.scss';
 
 export class ItemOrderCard extends Component {
+	render() {
+		const {imageSrc, rarity} = this.props;
 
+		return (
+			<div className={'item-order-card'}>
+				<ItemCoin
+					imageSrc={imageSrc}
+					rarity={rarity}
+				/>
+			</div>
+		);
+	}
 }
+
+ItemOrderCard.propTypes = {
+	index: PropTypes.number.isRequired,
+	imageSrc: PropTypes.string.isRequired,
+	rarity: PropTypes.string.isRequired,
+};
 
 export default ItemOrderCard;
