ddabebeebcb4fd1fbb94ec0554dd0408a9906de5
Marcin Szuster <szust.marcin@gmail.com>
2018-08-28 20:40:49 +0200

    Added opening Order after click OrderOpenButton

--- src/components/OrderOpenButton/OrderOpenButton.js
+++ src/components/OrderOpenButton/OrderOpenButton.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import {openOrderList} from '../../redux/actions';
 
 import './styles.scss';
 
 export class OrderOpenButton extends Component {
 	render() {
 		return (
-			<div className={'order-open-button'}>
+			<div
+				className={'order-open-button'}
+				onClick={this.props.openOrderList}
+			>
 				<div className={'material-icons'}>
 					<span className={'order-open-button--icon'}>shopping_basket</span>
 				</div>
@@ -15,4 +21,10 @@ export class OrderOpenButton extends Component {
 	}
 }
 
-export default OrderOpenButton;
+OrderOpenButton.propTypes = {
+	openOrderList: PropTypes.func //redux
+};
+
+const mapDispatchToProps = {openOrderList};
+
+export default connect(null, mapDispatchToProps)(OrderOpenButton);
