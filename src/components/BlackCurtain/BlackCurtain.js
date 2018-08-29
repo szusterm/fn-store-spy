6fc74c0f0a82de6cb019c73e533d8f02080edfd0
Marcin Szuster <szust.marcin@gmail.com>
2018-08-29 12:37:27 +0200

    Added base BlackCurtain theme

--- src/components/BlackCurtain/BlackCurtain.js
+++ src/components/BlackCurtain/BlackCurtain.js
@@ -4,7 +4,21 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 export class BlackCurtain extends Component {
+	render() {
+		const {show, onClick} = this.props;
 
+		return (
+			<div
+				className={`black-curtain ${(show) && 'black-curtain--showed'}`}
+				onClick={onClick}
+			/>
+		);
+	}
 }
+
+BlackCurtain.propTypes = {
+	show: PropTypes.bool.isRequired,
+	onClick: PropTypes.func
+};
 
 export default BlackCurtain;
