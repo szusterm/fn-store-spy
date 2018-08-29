3750f255e26574948aa758c5f4a8358d29d3b285
Marcin Szuster <szust.marcin@gmail.com>
2018-08-29 19:48:32 +0200

    Completed CircleOrderCount

--- src/components/CircleOrderCount/CircleOrderCount.js
+++ src/components/CircleOrderCount/CircleOrderCount.js
@@ -3,7 +3,21 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
 export class CircleOrderCount extends Component {
+	render() {
+		const {items} = this.props;
 
+		return (
+			<div className={'circle-order-count'}>
+				<span className={'circle-order-count--number'}>
+					{(items.length < 9) ? items.length : '9+'}
+				</span>
+			</div>
+		);
+	}
+}
+
+CircleOrderCount.propTypes = {
+	items: PropTypes.array //redux
 }
 
 const mapStateToProps = (state) => {
