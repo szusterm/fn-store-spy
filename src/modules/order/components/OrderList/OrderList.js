03268cb45694068474d15b5448b59a78acc37a25
Marcin Szuster <szust.marcin@gmail.com>
2018-08-31 21:07:47 +0200

    Updated OrderList to show all the order

--- src/modules/order/components/OrderList/OrderList.js
+++ src/modules/order/components/OrderList/OrderList.js
@@ -2,10 +2,51 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
+import ItemOrderCard from '../ItemOrderCard';
+
 import './styles.scss';
 
 export class OrderList extends Component {
+	getItemColsWidth() {
+		const {items} = this.props;
+		return (items.length <= 9) ? 4 : 3;
+	}
 
+	render() {
+		const {items} = this.props;
+
+		return (
+			<div>
+				{
+					items.map((item, index) => {
+						const {_id, imageSrc, rarity} = item;
+
+						return (
+							<div
+								key={_id}
+								className={`col-${this.getItemColsWidth()} list--item-box`}
+							>
+								<ItemOrderCard
+									index={index}
+									imageSrc={imageSrc}
+									rarity={rarity}
+								/>
+							</div>
+						);
+					})
+				}
+			</div>
+		);
+	}
 }
 
-export default OrderList;
+OrderList.propTypes = {
+	items: PropTypes.array //redux
+};
+
+const mapStateToProps = (state) => {
+	const {items} = state.order;
+	return {items};
+};
+
+export default connect(mapStateToProps, null)(OrderList);
