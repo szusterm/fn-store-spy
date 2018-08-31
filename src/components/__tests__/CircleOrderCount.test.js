999702f671a2fdb63c70204c39c1af2895a24dd3
Marcin Szuster <szust.marcin@gmail.com>
2018-08-31 15:25:57 +0200

    Changed variable name in tests file

--- src/components/__tests__/CircleOrderCount.test.js
+++ src/components/__tests__/CircleOrderCount.test.js
@@ -19,10 +19,10 @@ describe('CircleOrderCount Component', () => {
 			items: ['item0', 'item4', 'item2']
 		});
 
-		const showedNumber = wrapper.find('.circle-order-count--number').text();
+		const showedText = wrapper.find('.circle-order-count--number').text();
 		const itemsCountAsString = String(props.items.length);
 
-		expect(showedNumber).toBe(itemsCountAsString);
+		expect(showedText).toBe(itemsCountAsString);
 	});
 
 	it('if order length is higher than 9, shows "9+"', () => {
