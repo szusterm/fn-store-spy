25a0337be7f86556b1c73689f9015bc3f89a3ba1
Marcin Szuster <szust.marcin@gmail.com>
2018-08-31 21:03:50 +0200

    Added base OrderList component

---
+++ src/modules/order/components/OrderList/OrderList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import './styles.scss';
+
+export class OrderList extends Component {
+
+}
+
+export default OrderList;
