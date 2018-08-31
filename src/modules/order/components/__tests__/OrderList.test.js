80f0c7e37edc0896f2531afb157f8f34678f5026
Marcin Szuster <szust.marcin@gmail.com>
2018-08-31 21:38:19 +0200

    Added function to generate example items in OrderList tests

--- src/modules/order/components/__tests__/OrderList.test.js
+++ src/modules/order/components/__tests__/OrderList.test.js
@@ -3,8 +3,23 @@ import {shallow} from 'enzyme';
 
 import {OrderList} from '../OrderList/OrderList';
 
+const generateExampleItems = (length) => {
+	let exampleItems = [];
+	for (let index = 0; index < length; index++) {
+		exampleItems.push({
+			_id: `item${index}`,
+			imageSrc: 'https://example.com',
+			rarity: 'legendary'
+		});
+	}
+
+	return exampleItems;
+};
+
 const setup = (propOverrides) => {
-	const props = Object.assign({}, propOverrides);
+	const props = Object.assign({
+		items: generateExampleItems(2)
+	}, propOverrides);
 
 	const wrapper = shallow(<OrderList {...props}/>);
 
