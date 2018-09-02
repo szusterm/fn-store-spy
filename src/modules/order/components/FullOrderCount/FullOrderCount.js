e09b09137872f916a86f6c244fefdd77032f68e2
Marcin Szuster <szust.marcin@gmail.com>
2018-09-02 22:32:28 +0200

    Updated FullOrderCount

--- src/modules/order/components/FullOrderCount/FullOrderCount.js
+++ src/modules/order/components/FullOrderCount/FullOrderCount.js
@@ -5,7 +5,29 @@ import {connect} from 'react-redux';
 import './styles.scss';
 
 export class FullOrderCount extends Component {
+	checkThatOrderIsTooBig() {
+		const {orderedItems, maxItemsInOrder} = this.props;
 
+		return (orderedItems.length > maxItemsInOrder);
+	}
+
+	render() {
+		const {orderedItems, maxItemsInOrder} = this.props;
+
+		return (
+			<div className={'full-order-count'}>
+				<span
+					className={`full-order-count--ordered ${(this.checkThatOrderIsTooBig()) && 'full-order-count--ordered--exceeded'}`}
+				>
+					{orderedItems.length}
+				</span>
+				<span>/</span>
+				<span className={'full-order-count--maximum'}>
+					{maxItemsInOrder}
+				</span>
+			</div>
+		);
+	}
 }
 
 FullOrderCount.propTypes = {
