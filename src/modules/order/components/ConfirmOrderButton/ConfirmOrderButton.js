a7839d81a620fc51aa3861e4580cd886fb004887
Marcin Szuster <szust.marcin@gmail.com>
2018-09-02 10:24:00 +0200

    Added base OrderConfirmButton theme

--- src/modules/order/components/ConfirmOrderButton/ConfirmOrderButton.js
+++ src/modules/order/components/ConfirmOrderButton/ConfirmOrderButton.js
@@ -4,7 +4,13 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 export class ConfirmOrderButton extends Component {
-
+	render() {
+		return (
+			<button className={'confirm-order-button'}>
+				Create order
+			</button>
+		);
+	}
 }
 
 export default ConfirmOrderButton;
