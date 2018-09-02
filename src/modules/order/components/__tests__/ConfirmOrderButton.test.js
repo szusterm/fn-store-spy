de66300697b0062df835571d85329d634f9e5cb6
Marcin Szuster <szust.marcin@gmail.com>
2018-09-02 20:19:17 +0200

    Added ConfirmOrderButton tests

--- src/modules/order/components/__tests__/ConfirmOrderButton.test.js
+++ src/modules/order/components/__tests__/ConfirmOrderButton.test.js
@@ -20,4 +20,30 @@ describe('CircleOrderCount Component', () => {
 
 		expect(wrapper).toMatchSnapshot();
 	});
+
+	it('makes disabled if the order is empty', () => {
+		const {wrapper} = setup();
+
+		wrapper.setProps({orderedItems: ['item5']});
+		const disabledIfNotEmpty = wrapper.find('button').props().disabled;
+		expect(disabledIfNotEmpty).toBe(false);
+
+		wrapper.setProps({orderedItems: []});
+		const disabledIfEmpty = wrapper.find('button').props().disabled;
+		expect(disabledIfEmpty).toBe(true);
+	});
+
+	it('makes disabled, if a number of ordered items is higher than max', () => {
+		const {wrapper} = setup({
+			maxItemsInOrder: 2
+		});
+
+		wrapper.setProps({orderedItems: ['item5']});
+		const disabledIfNotHigher = wrapper.find('button').props().disabled;
+		expect(disabledIfNotHigher).toBe(false);
+
+		wrapper.setProps({orderedItems: ['item1', 'item2', 'item3']});
+		const disabledIfHigher = wrapper.find('button').props().disabled;
+		expect(disabledIfHigher).toBe(true);
+	});
 });
