a3d44c8c6bf494010a3c8046bae081a9817d7cb0
Marcin Szuster <szust.marcin@gmail.com>
2018-09-02 20:24:53 +0200

    Connected state to ConfirmOrderButton

--- src/modules/order/components/ConfirmOrderButton/ConfirmOrderButton.js
+++ src/modules/order/components/ConfirmOrderButton/ConfirmOrderButton.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
 import './styles.scss';
 
@@ -13,4 +14,19 @@ export class ConfirmOrderButton extends Component {
 	}
 }
 
-export default ConfirmOrderButton;
+ConfirmOrderButton.propTypes = {
+	orderedItems: PropTypes.array, //redux
+	maxItemsInOrder: PropTypes.number //redux
+};
+
+const mapStateToProps = (state) => {
+	const {items} = state.order;
+	const {maxItemsInOrder} = state.config;
+
+	return {
+		orderedItems: items,
+		maxItemsInOrder
+	};
+};
+
+export default connect(mapStateToProps, null)(ConfirmOrderButton);
