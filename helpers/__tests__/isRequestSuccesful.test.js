fd9adc2698e5e9faa02aa9c850d1f4daeb42d11a
Marcin Szuster <szust.marcin@gmail.com>
2018-09-04 12:16:58 +0200

    Added test for isRequestSuccessful()

---
+++ helpers/__tests__/isRequestSuccesful.test.js
@@ -0,0 +1,13 @@
+const isRequestSuccessful = require('../isRequestSuccesful');
+
+describe('isRequestSuccessful()', () => {
+	it('returns true if request was successful', () => {
+		const statusCodes = {
+			success: 200,
+			error: 500
+		};
+
+		expect(isRequestSuccessful(statusCodes.success)).toBe(true);
+		expect(isRequestSuccessful(statusCodes.error)).toBe(false);
+	});
+});
