6e02e23f4bbfeadebfd5d8a483c7e7e54db47c55
Marcin Szuster <szust.marcin@gmail.com>
2018-09-04 12:27:07 +0200

    Moved spi functions tests to API group

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -3,44 +3,46 @@ import axios from 'axios';
 
 jest.mock('axios');
 
-describe('fetchItems()', () => {
-	const params = {
-		name: 'Scout',
-		type: 'outfit',
-		page: 3
-	};
-
-	it('gets items from server', async () => {
-		const items = ['item0', 'item1'];
-
-		axios.mockReturnValueOnce(items);
-
-		const response = await api.fetchItems(params);
-
-		expect(response).toEqual(items);
-	});
-
-	it('call request with data', async () => {
-		const dataCalledWithAxios = {
-			method: 'get',
-			url: '/items/get',
-			params
+describe('API', () => {
+	describe('fetchItems()', () => {
+		const params = {
+			name: 'Scout',
+			type: 'outfit',
+			page: 3
 		};
 
-		await api.fetchItems(params);
+		it('gets items from server', async () => {
+			const items = ['item0', 'item1'];
 
-		expect(axios).toHaveBeenCalledWith(dataCalledWithAxios);
+			axios.mockReturnValueOnce(items);
+
+			const response = await api.fetchItems(params);
+
+			expect(response).toEqual(items);
+		});
+
+		it('call request with data', async () => {
+			const dataCalledWithAxios = {
+				method: 'get',
+				url: '/items/get',
+				params
+			};
+
+			await api.fetchItems(params);
+
+			expect(axios).toHaveBeenCalledWith(dataCalledWithAxios);
+		});
 	});
-});
 
-describe('fetchConfig()', () => {
-	it('gets items from server', async () => {
-		const config = {length: 5, items: 10};
+	describe('fetchConfig()', () => {
+		it('gets items from server', async () => {
+			const config = {length: 5, items: 10};
 
-		axios.mockReturnValueOnce(config);
+			axios.mockReturnValueOnce(config);
 
-		const response = await api.fetchConfig();
+			const response = await api.fetchConfig();
 
-		expect(response).toEqual(config);
+			expect(response).toEqual(config);
+		});
 	});
 });
