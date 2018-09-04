f1f2f1906a31cf8af08e0c1520d3cd44aecb8919
Marcin Szuster <szust.marcin@gmail.com>
2018-09-04 14:38:42 +0200

    Added api function to adding new order

--- src/api.js
+++ src/api.js
@@ -31,6 +31,30 @@ export const fetchItems = async (data) => {
 	}
 };
 
+export const addOrder = async (itemsIds) => {
+	try {
+		const response = await axios({
+			method: 'post',
+			url: '/order/add',
+			params: {
+				itemsIds
+			}
+		});
+
+		const requestSuccessful = isRequestSuccessful(response.status);
+
+		if (requestSuccessful) {
+			return getResponseObject(false, response.data);
+		}
+		else {
+			throw new Error();
+		}
+	}
+	catch (error) {
+		return getResponseObject(true, error);
+	}
+};
+
 export const fetchConfig = async () => {
 	try {
 		const {data} = await axios({
