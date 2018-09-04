f0332120f731aee24869af7d880333723b9e28a4
Marcin Szuster <szust.marcin@gmail.com>
2018-09-04 22:43:13 +0200

    Changed api module functions to one exported object as default

--- src/api.js
+++ src/api.js
@@ -3,57 +3,61 @@ import axios from 'axios';
 import getResponseObject from '../helpers/getResponseObject';
 import isRequestSuccessful from '../helpers/isRequestSuccessful';
 
-export const callApiRequest = async (data) => {
-	try {
-		const response = await axios(data);
+class Api {
+	async callApiRequest(data) {
+		try {
+			const response = await axios(data);
 
-		const requestSuccessful = isRequestSuccessful(response.status);
+			const requestSuccessful = isRequestSuccessful(response.status);
 
-		if (requestSuccessful) {
-			return getResponseObject(false, response.data);
+			if (requestSuccessful) {
+				return getResponseObject(false, response.data);
+			}
+			else {
+				throw new Error(response.data);
+			}
 		}
-		else {
-			throw new Error(response.data);
+		catch (error) {
+			return getResponseObject(true, error.message);
 		}
 	}
-	catch (error) {
-		return getResponseObject(true, error.message);
+
+	async fetchItems(data) {
+		const {name, type, page} = data;
+
+		const requestData = {
+			method: 'get',
+			url: '/items/get',
+			params: {
+				name,
+				type,
+				page
+			}
+		};
+
+		return await this.callApiRequest(requestData);
 	}
-};
 
-export const fetchItems = async (data) => {
-	const {name, type, page} = data;
+	async addOrder(itemsIds) {
+		const requestData = {
+			method: 'post',
+			url: '/order/add',
+			params: {
+				itemsIds
+			}
+		};
 
-	const requestData = {
-		method: 'get',
-		url: '/items/get',
-		params: {
-			name,
-			type,
-			page
-		}
-	};
+		return await this.callApiRequest(requestData);
+	}
 
-	return await callApiRequest(requestData);
-};
+	async fetchConfig() {
+		const requestData = {
+			method: 'get',
+			url: '/config'
+		};
 
-export const addOrder = async (itemsIds) => {
-	const requestData = {
-		method: 'post',
-		url: '/order/add',
-		params: {
-			itemsIds
-		}
-	};
+		return await this.callApiRequest(requestData);
+	}
+}
 
-	return await callApiRequest(requestData);
-};
-
-export const fetchConfig = async () => {
-	const requestData = {
-		method: 'get',
-		url: '/config'
-	};
-
-	return await callApiRequest(requestData);
-};
+export default new Api();
