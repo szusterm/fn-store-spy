3960e5c8e52aaf53a24365af0c4c786f16e4174d
Marcin Szuster <szust.marcin@gmail.com>
2018-09-04 23:22:38 +0200

    Added breaks between actions

--- src/modules/config/reducer.js
+++ src/modules/config/reducer.js
@@ -6,6 +6,7 @@ const reducer = (state = initialState, action) => {
 	switch(action.type) {
 		case SET_CONFIG:
 			return {...state, ...action.payload};
+
 		default:
 			return state;
 	}
--- src/modules/order/reducer.js
+++ src/modules/order/reducer.js
@@ -14,27 +14,32 @@ const reducer = (state = initialState, action) => {
 				...state,
 				items: [...state.items, payload]
 			};
+
 		case types.REMOVE_ITEM_BY_INDEX:
 			state.items.splice(payload, 1);
 			return {
 				...state,
 				items: [...state.items]
 			};
+
 		case types.CLEAR_ITEMS:
 			return {
 				...state,
 				items: []
 			};
+
 		case types.OPEN_ORDER_LIST:
 			return {
 				...state,
 				listOpened: true
 			};
+
 		case types.CLOSE_ORDER_LIST:
 			return {
 				...state,
 				listOpened: false
 			};
+
 		default:
 			return state;
 	}
