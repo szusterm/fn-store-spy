93b4c3d5541844feb8d0052492f9f02f563936f8
Marcin Szuster <szust.marcin@gmail.com>
2018-09-04 12:55:21 +0200

    Updated detchItems() api to a new response

--- src/api.js
+++ src/api.js
@@ -1,17 +1,34 @@
 import axios from 'axios';
 
+import getResponseObject from '../helpers/getResponseObject';
+import isRequestSuccessful from '../helpers/isRequestSuccessful';
+
 export const fetchItems = async (data) => {
 	const {name, type, page} = data;
 
-	return await axios({
-		method: 'get',
-		url: '/items/get',
-		params: {
-			name,
-			type,
-			page
+	try {
+		const response = await axios({
+			method: 'get',
+			url: '/items/get',
+			params: {
+				name,
+				type,
+				page
+			}
+		});
+
+		const requestSuccessful = isRequestSuccessful(response.status);
+
+		if (requestSuccessful) {
+			return getResponseObject(false, response.data);
 		}
-	});
+		else {
+			throw new Error();
+		}
+	}
+	catch (error) {
+		return getResponseObject(true, error);
+	}
 };
 
 export const fetchConfig = async () => {
