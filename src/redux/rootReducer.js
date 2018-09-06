5cabc61cbf3cd0cfc6f0a17e6b816d45e7429f3d
Marcin Szuster <szust.marcin@gmail.com>
2018-09-06 22:10:02 +0200

    Connected ending reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 
 import searching from '../modules/searching/reducer';
 import order from '../modules/order/reducer';
+import ending from '../modules/ending/reducer';
 import config from '../modules/config/reducer';
 
 export default combineReducers({
 	searching,
 	order,
+	ending,
 	config
 });
