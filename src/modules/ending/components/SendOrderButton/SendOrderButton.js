69364fbd2f0dce6e1cf23b8c9fc11d82a84d1cba
Marcin Szuster <szust.marcin@gmail.com>
2018-09-06 22:29:21 +0200

    Fixed action creator import in SendOrderButton

--- src/modules/ending/components/SendOrderButton/SendOrderButton.js
+++ src/modules/ending/components/SendOrderButton/SendOrderButton.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
-import {sendOrder} from '../../../order/actions';
+import {sendOrder} from '../../actions';
 
 import './styles.scss';
 
