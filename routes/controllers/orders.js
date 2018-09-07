9a929d0d580fd6f47b6a2f7479356a2e90b36aef
Marcin Szuster <szust.marcin@gmail.com>
2018-09-07 23:17:38 +0200

    Added order controller

---
+++ routes/controllers/orders.js
@@ -0,0 +1,13 @@
+const order = require('../../database/factories/order');
+
+export const saveOrder = async (req, res) => {
+	const {itemsIds} = req.body;
+	const {err, data} = await order.add(itemsIds, 'fn4rt7');
+
+	if (!err) {
+		res.json(data);
+	}
+	else {
+		res.status(500).send(false);
+	}
+};
