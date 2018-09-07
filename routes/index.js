8fc5730fb761de16fb4c6b504dc85f9c103bc0e7
Marcin Szuster <szust.marcin@gmail.com>
2018-09-07 23:29:31 +0200

    Removed old routes

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const clientConfig = require('../config/client');
-
-router.get('/config', (req, res) => res.json(clientConfig));
-
-module.exports = router;
