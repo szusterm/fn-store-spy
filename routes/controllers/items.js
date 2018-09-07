7690d973bb4bc7e458082d4357854026e84d536f
Marcin Szuster <szust.marcin@gmail.com>
2018-09-07 23:16:18 +0200

    Added items controller

---
+++ routes/controllers/items.js
@@ -0,0 +1,18 @@
+const item = require('../../database/factories/item');
+
+export const getItems = async (req, res) => {
+	const {name, type, page} = req.query;
+
+	const {err, data} = await item.find()
+		.name(name)
+		.type(type)
+		.page(page)
+		.exec();
+
+	if (!err) {
+		res.json(data);
+	}
+	else {
+		res.status(500).send(false);
+	}
+};
