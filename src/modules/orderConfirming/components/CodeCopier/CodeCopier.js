bc930a89fc81644461354c1313212de11bf478d5
Marcin Szuster <szust.marcin@gmail.com>
2018-09-07 17:28:11 +0200

    Added react-copy-to-clipboard module

--- src/modules/orderConfirming/components/CodeCopier/CodeCopier.js
+++ src/modules/orderConfirming/components/CodeCopier/CodeCopier.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {CopyToClipBoard} from 'react-copy-to-clipboard';
 
 import './styles.scss';
 
