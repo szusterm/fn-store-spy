264d2a5d96257c70a18c27da51436fbd5ab32cbe
Marcin Szuster <szust.marcin@gmail.com>
2018-09-07 12:58:07 +0200

    Changed sent code in adding the order route

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,7 @@ const order = require('../database/factories/order');
 
 router.post('/add', async (req, res) => {
 	const {itemsIds} = req.body;
-	const {err, data} = await order.add(itemsIds, 'code');
+	const {err, data} = await order.add(itemsIds, 'fn4rt7');
 
 	if (!err) {
 		res.json(data);
