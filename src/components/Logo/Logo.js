e63d541563d2b5dad58a301af2cc367197072e48
Marcin Szuster <szust.marcin@gmail.com>
2018-09-08 17:16:32 +0200

    Added different logo colors which depends on prop

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import whiteLogoUrl from '../../images/logo-white.png';
+import blackLogoUrl from '../../images/logo-black.png';
 
 import './styles.scss';
 
 export class Logo extends Component {
 	render() {
+		const {color} = this.props;
+
 		return (
 			<div className={'start-info'}>
-				<img src={whiteLogoUrl}/>
+				<img src={(color === 'white') ? whiteLogoUrl : blackLogoUrl}/>
 			</div>
 		);
 	}
 }
+
+Logo.propTypes = {
+	color: PropTypes.string.isRequired
+};
 
 export default Logo;
