0b99ecceb30942b2eb93a4e978c364a3e43fc3e9
Marcin Szuster <szust.marcin@gmail.com>
2018-09-08 18:55:20 +0200

    Imported Logo to StartInfo

--- src/components/StartInfo/StartInfo.js
+++ src/components/StartInfo/StartInfo.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
 
+import Logo from '../Logo';
+
 import './styles.scss';
 
 export class StartInfo extends Component {
 	render() {
 		return (
 			<header className={'start-info'}>
-				<div className={'container'}>
-					<div className={'row start-info--description'}>
-						<h1 className={'description--text'}>
-							<span className={'description--text--bold'}>Hello. </span>
-							Welcome to StoreSpy.<br/>
-							Messenger shop notifier.
-						</h1>
-					</div>
+				<div className={'start-info--description'}>
+					<Logo color={'white'}/>
+					<h1 className={'description--text'}>
+						<span className={'description--text--bold'}>Hello. </span>
+						Welcome to StoreSpy.<br/>
+						Messenger shop notifier.
+					</h1>
 				</div>
 			</header>
 		);
