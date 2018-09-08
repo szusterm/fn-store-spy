49d475af6523efe14696c393c45f28419b966d18
Marcin Szuster <szust.marcin@gmail.com>
2018-09-08 18:57:41 +0200

    Changed classes in StartInfo

--- src/components/StartInfo/StartInfo.js
+++ src/components/StartInfo/StartInfo.js
@@ -7,11 +7,13 @@ import './styles.scss';
 export class StartInfo extends Component {
 	render() {
 		return (
-			<header className={'start-info'}>
-				<div className={'start-info--description'}>
-					<Logo color={'white'}/>
-					<h1 className={'description--text'}>
-						<span className={'description--text--bold'}>Hello. </span>
+			<header>
+				<div className={'start-info'}>
+					<div className={'start-info--logo-box'}>
+						<Logo color={'white'}/>
+					</div>
+					<h1 className={'start-info--text'}>
+						<span className={'start-info--text--bold'}>Hello. </span>
 						Welcome to StoreSpy.<br/>
 						Messenger shop notifier.
 					</h1>
