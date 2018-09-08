210d3296eca6f81fd1bb0e22fa8e00b30a3a7d74
Marcin Szuster <szust.marcin@gmail.com>
2018-09-08 16:56:15 +0200

    Removed useless indents

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
+				use: {loader: 'babel-loader'}
 			},
 			{
 				test: /\.scss$/,
