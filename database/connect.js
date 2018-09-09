c5e1c15cbabd80e95e828d3b3f94388db381ab25
Marcin Szuster <szust.marcin@gmail.com>
2018-09-09 11:38:37 +0200

    Updated generating db connection url to use env vars

--- database/connect.js
+++ database/connect.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-const config = require('../config/database');
-
-const databaseUrl = `mongodb://${config.user}:${config.password}@${config.url}`;
+const databaseUrl = require('./getConnectionUrl')();
 
 module.exports = () => {
+	const {DB_URL} = process.env;
+
 	mongoose.connect(databaseUrl, {useNewUrlParser: true});
 
 	const db = mongoose.connection;
 
 	db.on('error', console.error.bind(console, 'Mongoose connection error:'));
 
-	db.once('open', () => console.log(`Mongoose connected to: ${config.url}`));
+	db.once('open', () => console.log(`Mongoose connected to: ${DB_URL}`));
 
-	db.on('disconnected', () => console.log('Mongoose disconnected'))
+	db.on('disconnected', () => console.log('Mongoose disconnected'));
 };
