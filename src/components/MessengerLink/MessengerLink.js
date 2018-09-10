847373da629a506059c7181a1c910054443e224e
Marcin Szuster <szust.marcin@gmail.com>
2018-09-10 13:16:36 +0200

    Updated MessengerLink styles

--- src/components/MessengerLink/MessengerLink.js
+++ src/components/MessengerLink/MessengerLink.js
@@ -3,7 +3,13 @@ import React, {Component} from 'react';
 import './styles.scss';
 
 export class MessengerLink extends Component {
-
+	render() {
+		return (
+			<a className={'messenger-link'} href={'#'}>
+				m.me/ssbot
+			</a>
+		);
+	}
 }
 
 export default MessengerLink;
