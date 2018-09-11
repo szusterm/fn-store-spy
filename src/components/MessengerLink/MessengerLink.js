b8737d6cb76033a79fe96b6a3d93559e981c9876
Marcin Szuster <szust.marcin@gmail.com>
2018-09-11 13:47:07 +0200

    Updated MessengerLink theme

--- src/components/MessengerLink/MessengerLink.js
+++ src/components/MessengerLink/MessengerLink.js
@@ -5,13 +5,13 @@ import './styles.scss';
 export class MessengerLink extends Component {
 	render() {
 		return (
-			<div className={'col-3'}>
+			<div className={'col-12 col-sm-6 col-md-4 col-lg-3'}>
 				<a
 					className={'messenger-link'}
 					href={'#'}
 				>
 					<div className={'messenger-link--button'}>
-						m.me/ssbot
+						Messenger Bot
 					</div>
 				</a>
 			</div>
