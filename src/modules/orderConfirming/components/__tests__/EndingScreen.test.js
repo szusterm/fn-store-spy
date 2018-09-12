ae8f58edf234f1a274756338081a09050d2cdd61
Marcin Szuster <szust.marcin@gmail.com>
2018-09-12 12:36:49 +0200

    Updated EndingScreen tests

--- src/modules/orderConfirming/components/__tests__/EndingScreen.test.js
+++ src/modules/orderConfirming/components/__tests__/EndingScreen.test.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import CodeCopier from '../CodeCopier';
+import CancelButton from '../../../../components/CancelButton';
+import BlackCurtain from '../../../../components/BlackCurtain';
 
 import {EndingScreen} from '../EndingScreen/EndingScreen';
 
 const setup = (propOverrides) => {
 	const props = Object.assign({
-		endingScreenOpened: true //redux
+		endingScreenOpened: true, //redux
+		hideEndingScreen: () => true
 	}, propOverrides);
 
 	const wrapper = shallow(<EndingScreen {...props}/>);
@@ -38,5 +41,35 @@ describe('EndingScreen Component', () => {
 		const {wrapper} = setup();
 
 		expect(wrapper.find(CodeCopier).exists()).toBe(true);
+	});
+
+	it('hides if BlackCurtain calls onClick callback', () => {
+		const {wrapper, props} = setup({
+			hideEndingScreen: jest.fn()
+		});
+
+		wrapper.find(BlackCurtain).props().onClick();
+
+		expect(props.hideEndingScreen).toHaveBeenCalledTimes(1);
+	});
+
+	it('puts listOpened from a redux state as a prop to BlackCurtain', () => {
+		const {wrapper, props} = setup({
+			endingScreenOpened: false
+		});
+
+		const showProp = wrapper.find(BlackCurtain).props().show;
+
+		expect(showProp).toBe(props.endingScreenOpened);
+	});
+
+	it('hides if CancelButton calls onClick callback', () => {
+		const {wrapper, props} = setup({
+			hideEndingScreen: jest.fn()
+		});
+
+		wrapper.find(CancelButton).props().onClick();
+
+		expect(props.hideEndingScreen).toHaveBeenCalledTimes(1);
 	});
 });
