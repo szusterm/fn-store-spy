5dfcd30b7cbcddd37bce2e35c7695e96973d9988
Marcin Szuster <szust.marcin@gmail.com>
2018-09-12 13:08:50 +0200

    Updated import in CodeCopier tests

--- src/modules/orderConfirming/components/__tests__/CodeCopier.test.js
+++ src/modules/orderConfirming/components/__tests__/CodeCopier.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 import {CodeCopier} from '../CodeCopier/CodeCopier';
 
