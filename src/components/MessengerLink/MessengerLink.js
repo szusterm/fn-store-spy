3420f1e030539404af3180b9d7492870bd8b68ab
Marcin Szuster <szust.marcin@gmail.com>
2018-09-12 19:01:14 +0200

    Added Messenger svg icon to MessengerLink

--- src/components/MessengerLink/MessengerLink.js
+++ src/components/MessengerLink/MessengerLink.js
@@ -10,7 +10,17 @@ export class MessengerLink extends Component {
 				href={'#'}
 			>
 				<div className={'messenger-link--button'}>
-					Go to Messenger
+					<svg
+						className={'messenger-link--svg-icon'}
+						xmlns={'http://www.w3.org/2000/svg'}
+						role={'img'}
+						viewBox={'0 0 213.25417 213.25417'}
+					>
+						<g transform={'translate(0 -83.746)'}>
+							<path d={'m106.63 83.746a106.63 106.63 0 0 0 -106.63 106.63 106.63 106.63 0 0 0 106.63 106.63 106.63 106.63 0 0 0 106.63 -106.63 106.63 106.63 0 0 0 -106.63 -106.63zm69.466 75.368-58.237 61.795-27.604-28.661-53.092 29.383 58.238-61.796 27.604 28.662z" fill="#00aad4'}/>
+						</g>
+					</svg>
+					<span>Go to Messenger</span>
 				</div>
 			</a>
 		);
