4e6280a90dfb275c8706bdbb9655066b5e3237bb
Marcin Szuster <szust.marcin@gmail.com>
2018-09-14 18:49:39 +0200

    Added endpoints for fnbr api

---
+++ services/fnbr/endpoints.js
@@ -0,0 +1,8 @@
+const FNBR_API_URL = 'https://fnbr.co/api';
+
+module.exports = {
+	images: `${FNBR_API_URL}/images`,
+	shop: `${FNBR_API_URL}/shop`,
+	stats: `${FNBR_API_URL}/stats`,
+	upcoming: `${FNBR_API_URL}/upcoming`
+};
