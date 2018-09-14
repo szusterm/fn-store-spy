43ab73807305757d05e0dc7d65eeb7655df88268
Marcin Szuster <szust.marcin@gmail.com>
2018-09-14 22:56:46 +0200

    Updated order schema
    Renamed id in items array to fnbrId

--- database/models/order.model.js
+++ database/models/order.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const OrderSchema = new mongoose.Schema({
 	code: String,
 	connected: Boolean,
-	items: [{id: String, done: Boolean}]
+	items: [{fnbrId: String, done: Boolean}]
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
