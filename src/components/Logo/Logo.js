8943da79ad6369a173952fcc18570c9a15be50af
Marcin Szuster <szust.marcin@gmail.com>
2018-09-14 12:30:36 +0200

    Moved images to directory with static files

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,20 +1,29 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import whiteLogoUrl from '../../images/logo-white.png';
-import blackLogoUrl from '../../images/logo-black.png';
-
 import './styles.scss';
 
 export class Logo extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			url: {
+				white: '../images/logo-white.png',
+				dark: '../images/logo-black.png'
+			}
+		};
+	}
+
 	render() {
+		const {url} = this.state;
 		const {dark} = this.props;
 
 		return (
 			<div className={'logo'}>
 				<img
 					className={'logo--image'}
-					src={(!dark) ? whiteLogoUrl : blackLogoUrl}
+					src={(!dark) ? url.white : url.dark}
 				/>
 			</div>
 		);
