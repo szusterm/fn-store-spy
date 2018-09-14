c18c336257b8c348cd061602862b12e7cc8805d6
Marcin Szuster <szust.marcin@gmail.com>
2018-09-14 22:55:51 +0200

    Updated item factory mock for new fnbrIds filter

--- database/factories/__mocks__/item.js
+++ database/factories/__mocks__/item.js
@@ -2,6 +2,7 @@ const item = {
 	find: jest.fn(() => item),
 	exec: jest.fn(() => item),
 	ids: jest.fn(() => item),
+	fnbrIds: jest.fn(() => item),
 	name: jest.fn(() => item),
 	names: jest.fn(() => item),
 	type: jest.fn(() => item),
