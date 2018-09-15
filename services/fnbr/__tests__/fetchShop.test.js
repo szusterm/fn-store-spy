fb205c61b53228376f0c09989905c3c7ec5ae0d4
Marcin Szuster <szust.marcin@gmail.com>
2018-09-15 22:09:28 +0200

    Changed fnbr api test file to tests for a single module with fetchShop()

---
+++ services/fnbr/__tests__/fetchShop.test.js
@@ -0,0 +1,25 @@
+const fetchShop = require('../fetchShop');
+const callApiRequest = require('../../../helpers/callApiRequest');
+
+jest.mock('../../helpers/callApiRequest');
+
+describe('fetchShop()', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('sends request through callApiRequest() to get shop items', async () => {
+		await fetchShop();
+
+		expect(callApiRequest).toHaveBeenCalledTimes(1);
+	});
+
+	it('returns callApiRequest() response', async () => {
+		const exampleResponse = 'resp';
+
+		callApiRequest.mockReturnValueOnce(Promise.resolve(exampleResponse));
+		const returnedResponse = await fetchShop();
+
+		expect(returnedResponse).toBe(exampleResponse);
+	});
+});
