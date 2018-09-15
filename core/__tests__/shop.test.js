f38f238673907cd613e55030ac9831cabfe59048
Marcin Szuster <szust.marcin@gmail.com>
2018-09-16 00:14:06 +0200

    Removed tests for non existing shop() methods

--- core/__tests__/shop.test.js
+++ core/__tests__/shop.test.js
@@ -53,66 +53,6 @@ describe('Shop Controller', () => {
 		});
 	});
 
-	describe('_updateItemsWithDatabase()', () => {
-		it('updates an offer with database data using an items names array', async () => {
-			const returnedResponse = {
-				err: false,
-				data: [
-					{id: '123'},
-					{id: '666'}
-				]
-			};
-
-			shop._items = [];
-
-			itemFactory.exec.mockReturnValueOnce(returnedResponse);
-
-			await shop._updateItemsWithDatabase([]);
-
-			expect(shop._items).toEqual(returnedResponse.data);
-		});
-
-		it('download items from a database', async () => {
-			const itemsNames = ['My axe', 'Super Boots'];
-
-			await shop._updateItemsWithDatabase(itemsNames);
-
-			expect(itemFactory.find).toHaveBeenCalledTimes(1);
-			expect(itemFactory.names).toHaveBeenCalledWith(itemsNames);
-			expect(itemFactory.exec).toHaveBeenCalledTimes(1);
-		});
-
-		it('returns boolean that an error occurred', async () => {
-			let response;
-
-			itemFactory.exec.mockReturnValueOnce({err: true, data: {}});
-			response = await shop._updateItemsWithDatabase([]);
-			expect(response).toBeFalsy();
-
-			itemFactory.exec.mockReturnValueOnce({err: false, data: {}});
-			response = await shop._updateItemsWithDatabase([]);
-			expect(response).toBeTruthy();
-
-		});
-	});
-
-	describe('_getNamesFromObjects()', () => {
-		it('returns an array of names which had been got from objects', () => {
-			const items = [
-				{name: 'Huge Axe'},
-				{name: 'Big Outfit'}
-			];
-			const itemsNames = [
-				items[0].name,
-				items[1].name
-			];
-
-			const response = shop._getNamesFromObjects(items);
-
-			expect(response).toEqual(itemsNames);
-		});
-	});
-
 	describe('GETTERS', () => {
 		const items = [
 			{
