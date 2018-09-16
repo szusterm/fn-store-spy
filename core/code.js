2c955918705be5c430497b2c81f2a133a36d1a07
Marcin Szuster <szust.marcin@gmail.com>
2018-09-16 20:37:51 +0200

    Added method to generate random number in Code()

--- core/code.js
+++ core/code.js
@@ -2,6 +2,10 @@ class Code {
 	constructor() {
 		this._availableChars = 'abcdefgijklmnopqrstuvwxyz0123456789';
 	}
+
+	_getRandomNumber(max) {
+		return Math.round(Math.random()*max);
+	}
 }
 
 module.exports = new Code();
