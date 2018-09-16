417fd3415d08821219554441bdcd4ed696c3fb46
Marcin Szuster <szust.marcin@gmail.com>
2018-09-16 21:57:52 +0200

    Added test for Code()._getRandomChar()

--- core/__tests__/code.test.js
+++ core/__tests__/code.test.js
@@ -10,6 +10,21 @@ describe('Code', () => {
 
 	afterEach(() => code = null);
 
+	describe('_getRandomChar()', () => {
+		let mockGetRandomNumber;
+		beforeEach(() => mockGetRandomNumber = jest.spyOn(code, '_getRandomNumber'));
+		afterEach(() => mockGetRandomNumber.mockRestore());
+
+		it('returns random char from an available collection', () => {
+			code._availableChars = 'abc';
+
+			const returnedChar = code._getRandomChar();
+
+			expect(mockGetRandomNumber).toHaveBeenCalledWith(2);
+			expect(returnedChar).toBe('b');
+		});
+	});
+
 	describe('_getRandomNumber', () => {
 		it('returns random number between 0 and number in param', () => {
 			const firstRandom = code._getRandomNumber(10000000);
