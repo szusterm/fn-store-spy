4727b012c4e0cd6076f3edb2f3808efe9be54c7d
Marcin Szuster <szust.marcin@gmail.com>
2018-09-16 21:47:27 +0200

    Added _getRandomChar() method to Code()

--- core/code.js
+++ core/code.js
@@ -3,6 +3,13 @@ class Code {
 		this._availableChars = 'abcdefgijklmnopqrstuvwxyz0123456789';
 	}
 
+	_getRandomChar() {
+		const lastCharIndex = this._availableChars.length - 1;
+		const randomCharIndex = this._getRandomNumber(lastCharIndex);
+
+		return this._availableChars.charAt(randomCharIndex);
+	}
+
 	_getRandomNumber(max) {
 		return Math.round(Math.random()*max);
 	}
