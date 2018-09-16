0974934ecb7d611ff58097a696974c6fd3e352df
Marcin Szuster <szust.marcin@gmail.com>
2018-09-16 22:46:10 +0200

    Added generate() method to Code()

--- core/code.js
+++ core/code.js
@@ -1,6 +1,19 @@
+const config = require('../config');
+const {codeLength} = config.ordering;
+
 class Code {
 	constructor() {
 		this._availableChars = 'abcdefgijklmnopqrstuvwxyz0123456789';
+	}
+
+	generate() {
+		let code = '';
+
+		for (let i = 1; i <= codeLength; i++) {
+			code += this._getRandomChar();
+		}
+
+		return code;
 	}
 
 	_getRandomChar() {
