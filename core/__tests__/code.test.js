1107fc616467b7f7440dd507c52cff13faedb65c
Marcin Szuster <szust.marcin@gmail.com>
2018-09-16 21:43:52 +0200

    Added tests for Code()._getRandomNumber()

--- core/__tests__/code.test.js
+++ core/__tests__/code.test.js
@@ -9,4 +9,19 @@ describe('Code', () => {
 	});
 
 	afterEach(() => code = null);
+
+	describe('_getRandomNumber', () => {
+		it('returns random number between 0 and number in param', () => {
+			const firstRandom = code._getRandomNumber(10000000);
+			const secondRandom = code._getRandomNumber(10000000);
+
+			expect(firstRandom).not.toBe(secondRandom);
+		});
+
+		it('returns rounded number', () => {
+			const random = code._getRandomNumber(1);
+
+			expect((random === 0 || random === 1)).toBe(true);
+		});
+	});
 });
