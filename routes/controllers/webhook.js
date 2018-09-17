426f460cbf4a2d6c4cfc926ae86c0540444da7dc
Marcin Szuster <szust.marcin@gmail.com>
2018-09-17 14:36:30 +0200

    Added webhook.verify() controller

--- routes/controllers/webhook.js
+++ routes/controllers/webhook.js
@@ -13,3 +13,22 @@ exports.receiveEvent = (req, res) => {
 		res.sendStatus(404);
 	}
 };
+
+exports.verify = (req, res) => {
+	const {MESSENGER_VERIFY_TOKEN} = process.env;
+
+	const mode = req.query['hub.mode'];
+	const token = req.query['hub.verify_token'];
+	const challenge = req.query['hub.challenge'];
+
+	if (mode && token) {
+		if (mode === 'subscribe' && token === MESSENGER_VERIFY_TOKEN) {
+			console.log('WEBHOOK_VERIFIED');
+
+			res.status(200).send(challenge);
+		}
+		else {
+			res.sendStatus(403);
+		}
+	}
+};
