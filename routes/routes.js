1bdfd11015e64efc2655e7de3599954cce7b14fd
Marcin Szuster <szust.marcin@gmail.com>
2018-09-17 14:24:42 +0200

    Added webhook.receiveEvent() controller to routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const itemsController = require('./controllers/items');
 const orderController = require('./controllers/order');
 const configController = require('./controllers/config');
+const webhookController = require('./controllers/webhook');
 
 router.get('/config', configController.getConfig);
 
 router.get('/items/get', itemsController.getItems);
 
 router.post('/order/add', orderController.saveOrder);
+
+router.post('/webhook', webhookController.receiveEvent);
 
 module.exports = router;
