8ab376e0a1c88d34464f765f0642dc52e91e63b8
Marcin Szuster <szust.marcin@gmail.com>
2018-09-17 19:06:29 +0200

    Added method to send a message in Messenger()

--- core/messenger.js
+++ core/messenger.js
@@ -1,6 +1,25 @@
+const code = require('./code');
+const callApiRequest = require('../helpers/callApiRequest');
+
 class Messenger {
-	send() {
-		console.log('Sending message');
+	async sendMessage(userId, message) {
+		const {FB_PAGE_ACCESS_TOKEN} = process.env;
+		const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${FB_PAGE_ACCESS_TOKEN}`;
+
+		const data = {
+			url,
+			method: 'post',
+			headers: {'content-type': 'application/json'},
+			data: {
+				messaging_type: '',
+				recipient: {id: userId},
+				message: {test: message}
+			}
+		};
+
+		const {err} = await callApiRequest(data);
+
+		return !err;
 	}
 }
 
