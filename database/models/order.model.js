db5efe78af6917c891f59c0df4b1fe73a58459fb
Marcin Szuster <szust.marcin@gmail.com>
2018-09-17 20:57:27 +0200

    Updated Order schema

--- database/models/order.model.js
+++ database/models/order.model.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
 	code: String,
-	connected: Boolean,
-	items: [{fnbrId: String, done: Boolean}]
+	connected: {type: Boolean, default: false},
+	userId: {type: String, default: ''},
+	items: [
+		{
+			fnbrId: String,
+			done: {type: Boolean, default: false}
+		}
+	],
+	date: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
