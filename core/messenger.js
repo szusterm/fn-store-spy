5c2d7f5aff468cdefd92542ea355744b7d4aaad4
Marcin Szuster <szust.marcin@gmail.com>
2018-09-18 12:59:17 +0200

    Moved Messenger().sendMessage() method up

--- core/messenger.js
+++ core/messenger.js
@@ -3,6 +3,25 @@ const code = require('./code');
 const callApiRequest = require('../helpers/callApiRequest');
 
 class Messenger {
+	async sendMessage(userId, message) {
+		const {FB_PAGE_ACCESS_TOKEN} = process.env;
+		const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${FB_PAGE_ACCESS_TOKEN}`;
+
+		const data = {
+			url,
+			method: 'post',
+			headers: {'content-type': 'application/json'},
+			data: {
+				recipient: {id: userId},
+				message: {text: message}
+			}
+		};
+
+		const response = await callApiRequest(data);
+
+		return (!response.err && data.recipient_id);
+	}
+
 	async handle(userId, message) {
 		if (code.isCode(message)) {
 			const receivedCode = code.removePrefix(message);
@@ -27,25 +46,6 @@ class Messenger {
 		else {
 			await this.sendMessage(userId, 'Some shit');
 		}
-	}
-
-	async sendMessage(userId, message) {
-		const {FB_PAGE_ACCESS_TOKEN} = process.env;
-		const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${FB_PAGE_ACCESS_TOKEN}`;
-
-		const data = {
-			url,
-			method: 'post',
-			headers: {'content-type': 'application/json'},
-			data: {
-				recipient: {id: userId},
-				message: {text: message}
-			}
-		};
-
-		const response = await callApiRequest(data);
-
-		return (!response.err && data.recipient_id);
 	}
 }
 
