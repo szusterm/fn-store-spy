ae58aadb48163b031b8e7a61322b77eef21452d4
Marcin Szuster <szust.marcin@gmail.com>
2018-09-20 22:37:53 +0200

    Updated order factory mock

--- database/factories/__mocks__/order.js
+++ database/factories/__mocks__/order.js
@@ -1,6 +1,13 @@
 const order = {
 	add: jest.fn(() => true),
-	findMatchingByFnbrIds: jest.fn(() => true)
+	update: jest.fn(() => ({
+		connect: jest.fn(() => true),
+		disconnect: jest.fn(() => true)
+	})),
+	find: jest.fn(() => ({
+		byCode: jest.fn(() => true),
+		matchingByFnbrIds: jest.fn(() => true)
+	}))
 };
 
 module.exports = order;
