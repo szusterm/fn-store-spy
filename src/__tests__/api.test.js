5b56b6c0f75ef0d8cb5e8ef3b7cc3743138dba54
Marcin Szuster <szust.marcin@gmail.com>
2018-09-21 17:10:04 +0200

    Updated front api tests

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -1,62 +1,12 @@
-import api from '../api';
-import axios from 'axios';
-import getResponseObject from '../../helpers/getResponseObject';
-import isRequestSuccessful from '../../helpers/isRequestSuccessful';
+import * as api from '../api';
 
-jest.mock('axios');
-jest.mock('../../helpers/getResponseObject');
-jest.mock('../../helpers/isRequestSuccessful');
+import callApiRequest from '../../helpers/callApiRequest';
+
+jest.mock('../../helpers/callApiRequest');
 
 describe('API', () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
-	});
-
-	describe('callApiRequest()', () => {
-		const exampleRequestData = {
-			method: 'get',
-			url: '/',
-			params: 'some params'
-		};
-
-		it('calls request with a passed data', async () => {
-			await api.callApiRequest(exampleRequestData);
-
-			expect(axios).toHaveBeenCalledWith(exampleRequestData);
-		});
-
-		it('returns an object with err false and response data, if it is no error', async () => {
-			const exampleResponse = {
-				status: 200,
-				data: 'example data'
-			};
-
-			axios.mockReturnValueOnce(Promise.resolve(exampleResponse));
-			isRequestSuccessful.mockReturnValueOnce(true);
-
-			await api.callApiRequest(exampleRequestData);
-
-			expect(getResponseObject).toHaveBeenCalledWith(false, exampleResponse.data);
-		});
-
-		it('returns an object with err true and error data, if it is error', async () => {
-			const exampleErrorResponse = {
-				status: 500,
-				data: 'example error'
-			};
-
-			axios.mockReturnValueOnce(Promise.resolve(exampleErrorResponse));
-			isRequestSuccessful.mockReturnValueOnce(false);
-			await api.callApiRequest(exampleRequestData);
-
-			expect(getResponseObject).toHaveBeenCalledWith(true, exampleErrorResponse.data);
-
-			axios.mockReturnValueOnce(Promise.reject(exampleErrorResponse));
-			isRequestSuccessful.mockReturnValueOnce(false);
-			await api.callApiRequest(exampleRequestData);
-
-			expect(getResponseObject).toHaveBeenCalledWith(true, exampleErrorResponse.data);
-		});
 	});
 
 	describe('fetchItems()', () => {
@@ -70,20 +20,16 @@ describe('API', () => {
 			}
 		};
 
-		let mockCallApiRequest;
-		beforeEach(() => mockCallApiRequest = jest.spyOn(api, 'callApiRequest'));
-		afterEach(() => mockCallApiRequest.mockRestore());
-
 		it('sends request through callApiRequest() with filters', async () => {
 			await api.fetchItems(requestData.params);
 
-			expect(mockCallApiRequest).toHaveBeenCalledWith(requestData);
+			expect(callApiRequest).toHaveBeenCalledWith(requestData);
 		});
 
 		it('returns callApiRequest() response', async () => {
 			const exampleResponse = 'resp';
 
-			mockCallApiRequest.mockReturnValueOnce(Promise.resolve(exampleResponse));
+			callApiRequest.mockReturnValueOnce(Promise.resolve(exampleResponse));
 			const returnedResponse = await api.fetchItems(requestData.params);
 
 			expect(returnedResponse).toBe(exampleResponse);
@@ -99,20 +45,16 @@ describe('API', () => {
 			}
 		};
 
-		let mockCallApiRequest;
-		beforeEach(() => mockCallApiRequest = jest.spyOn(api, 'callApiRequest'));
-		afterEach(() => mockCallApiRequest.mockRestore());
-
 		it('sends request through callApiRequest() with items ids', async () => {
 			await api.addOrder(requestData.data.itemsFnbrIds);
 
-			expect(mockCallApiRequest).toHaveBeenCalledWith(requestData);
+			expect(callApiRequest).toHaveBeenCalledWith(requestData);
 		});
 
 		it('returns callApiRequest() response', async () => {
 			const exampleResponse = 'resp';
 
-			mockCallApiRequest.mockReturnValueOnce(Promise.resolve(exampleResponse));
+			callApiRequest.mockReturnValueOnce(Promise.resolve(exampleResponse));
 			const returnedResponse = await api.addOrder(requestData.data.itemsIds);
 
 			expect(returnedResponse).toBe(exampleResponse);
@@ -125,20 +67,16 @@ describe('API', () => {
 			url: '/config'
 		};
 
-		let mockCallApiRequest;
-		beforeEach(() => mockCallApiRequest = jest.spyOn(api, 'callApiRequest'));
-		afterEach(() => mockCallApiRequest.mockRestore());
-
 		it('sends request through callApiRequest()', async () => {
 			await api.fetchConfig();
 
-			expect(mockCallApiRequest).toHaveBeenCalledWith(requestData);
+			expect(callApiRequest).toHaveBeenCalledWith(requestData);
 		});
 
 		it('returns callApiRequest() response', async () => {
 			const exampleResponse = 'resp';
 
-			mockCallApiRequest.mockReturnValueOnce(Promise.resolve(exampleResponse));
+			callApiRequest.mockReturnValueOnce(Promise.resolve(exampleResponse));
 			const returnedResponse = await api.fetchConfig();
 
 			expect(returnedResponse).toBe(exampleResponse);
