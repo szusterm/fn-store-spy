ce5f9ceedc58658a4440aaa2dbbf515a25e3c34c
Marcin Szuster <szust.marcin@gmail.com>
2018-09-26 14:02:25 +0200

    Updated Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,24 @@ import React, {Component} from 'react';
 import './styles.scss';
 
 export class Footer extends Component{
-
+	render() {
+		return (
+			<footer>
+				<div className={'footer'}>
+					<div className={'footer--row footer--row--fnbr'}>
+						<span>Items are delivered by </span>
+						<a
+							className={'row--fnbr-link'}
+							target={'_blank'}
+							href={'https://fnbr.co'}
+						>
+							fnbr.co
+						</a>
+					</div>
+				</div>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
